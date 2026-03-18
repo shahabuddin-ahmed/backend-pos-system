df7d9ecea898eecfc64e69775dcdf7d3bb03771a
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2026-03-18 14:27:13 +0600

    Implement inventory

--- src/web/router/v1/index.ts
+++ src/web/router/v1/index.ts
@@ -8,21 +8,26 @@ import { OutletControllerInterface } from "../../controller/v1/outlet";
 import { newOutletRouter } from "./outlet";
 import { newOutletMenuItemRouter } from "./outletMenuItem";
 import { OutletMenuItemControllerInterface } from "../../controller/v1/outletMenuItem";
+import { newInventoryRouter } from "./inventory";
+import { InventoryControllerInterface } from "../../controller/v1/inventory";
 
 export const newV1Router = async ({
     masterMenuItemController,
     outletController,
     outletMenuItemController,
+    inventoryController,
 }: {
     masterMenuItemController: MasterMenuItemControllerInterface;
     outletMenuItemController: OutletMenuItemControllerInterface;
     outletController: OutletControllerInterface;
+    inventoryController: InventoryControllerInterface;
 }): Promise<Router> => {
     const v1 = Router();
     v1.use("/health", await newHealthRouter());
     v1.use("/menu-items", await newMasterMenuItemRouter(masterMenuItemController));
     v1.use("/outlets", await newOutletRouter(outletController));
     v1.use("/outlet-menu-items", await newOutletMenuItemRouter(outletMenuItemController));
+    v1.use("/inventories", await newInventoryRouter(inventoryController));
 
     v1.use("*", (_req, res) => {
         console.log(`not_found_for_v1`, _req.method, _req.baseUrl);
